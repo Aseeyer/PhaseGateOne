40a6c45f958537a9654ebda0c5bcbce08750aba2
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-08-23 18:05:54 +0100

    added a function for calculating ovulation date

--- Java/MenstrualApp.java
+++ Java/MenstrualApp.java
@@ -5,4 +5,11 @@ public class MenstrualApp {
     public LocalDate calculateNextPeriodStart(LocalDate lastPeriodStart, int cycleLength) {
         return lastPeriodStart.plusDays(cycleLength);
     }
+
+
+    public LocalDate calculateOvulationDate(LocalDate lastPeriodStart, int averageCycleLength) {
+        LocalDate nextPeriod = calculateNextPeriodStart(lastPeriodStart, averageCycleLength);
+        return nextPeriod.minusDays(14);
+    }
+
 }
